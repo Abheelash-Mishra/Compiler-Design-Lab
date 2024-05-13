49d597a869287920c6f66a692669db7c846c27e1
Abheelash-Mishra <abheelashmishra@gmail.com>
2024-05-13 22:48:05 +0530

    Code updates

--- Exp 11 - Intermediate Code (Boolean Expressions)/Exp11.cpp
+++ Exp 11 - Intermediate Code (Boolean Expressions)/Exp11.cpp
@@ -5,41 +5,60 @@
 using namespace std;
 
 // Class to represent a token in the expression
-class Token {
+class Token
+{
 public:
-    enum Type { OPERAND, OPERATOR };
+    enum Type
+    {
+        OPERAND,
+        OPERATOR
+    };
     Type type;
     string value;
 
-    Token(Type t, const string& v) : type(t), value(v) {}
+    Token(Type t, const string &v) : type(t), value(v) {}
 };
 
 // Function to generate intermediate code for a boolean expression
-string generateIntermediateCode(const string& expression) {
+string generateIntermediateCode(const string &expression)
+{
     string intermediateCode;
 
     stack<Token> stack;
 
-    for (size_t i = 0; i < expression.size(); ++i) {
-        if (expression[i] == ' ') {
+    for (size_t i = 0; i < expression.size(); ++i)
+    {
+        if (expression[i] == ' ')
+        {
             continue; // Ignore whitespace
-        } else if (expression[i] == '(') {
+        }
+        else if (expression[i] == '(')
+        {
             stack.push(Token(Token::OPERATOR, "("));
-        } else if (expression[i] == ')') {
-            while (!stack.empty() && stack.top().value != "(") {
+        }
+        else if (expression[i] == ')')
+        {
+            while (!stack.empty() && stack.top().value != "(")
+            {
                 intermediateCode += stack.top().value + " ";
                 stack.pop();
             }
             stack.pop(); // Pop the '('
-        } else if (expression[i] == '&' || expression[i] == '|') {
-            while (!stack.empty() && stack.top().value != "(") {
+        }
+        else if (expression[i] == '&' || expression[i] == '|')
+        {
+            while (!stack.empty() && stack.top().value != "(")
+            {
                 intermediateCode += stack.top().value + " ";
                 stack.pop();
             }
             stack.push(Token(Token::OPERATOR, string(1, expression[i])));
-        } else {
+        }
+        else
+        {
             string operand;
-            while (i < expression.size() && isalnum(expression[i])) {
+            while (i < expression.size() && isalnum(expression[i]))
+            {
                 operand += expression[i];
                 ++i;
             }
@@ -48,7 +67,8 @@ string generateIntermediateCode(const string& expression) {
         }
     }
 
-    while (!stack.empty()) {
+    while (!stack.empty())
+    {
         intermediateCode += stack.top().value + " ";
         stack.pop();
     }
@@ -56,7 +76,8 @@ string generateIntermediateCode(const string& expression) {
     return intermediateCode;
 }
 
-int main() {
+int main()
+{
     string expression;
     cout << "Enter a boolean expression: ";
     getline(cin, expression);
